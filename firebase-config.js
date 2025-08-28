8a15f74cd5674c6d2e61148c72dcea80d813377b
MPOIA7 <nadiselayman@gmail.com>
2025-08-28 20:32:47 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js - Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjzn9vao9fDrPwpGWN0r-PD_d99gYDPGw",
+  authDomain: "to-do-list-219a5.firebaseapp.com",
+  projectId: "to-do-list-219a5",
+  storageBucket: "to-do-list-219a5.firebasestorage.app",
+  messagingSenderId: "566127528147",
+  appId: "1:566127528147:web:90a2e6f49f795deda36c26",
+  measurementId: "G-X86F6EDS51"
+};
+
+// Initialize Firebase will be done in app.js
+console.log('Firebase config loaded successfully');
